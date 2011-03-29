125c281bfbb2550e6b168c43bb66b0bd773282ba
mick <mick@z-inc.net>
2011-03-30 01:05:55 +0600

    more tests for Subscriber

--- server/src/test/scala/org/mtkachev/stomp/server/SubscriberSpecification.scala
+++ server/src/test/scala/org/mtkachev/stomp/server/SubscriberSpecification.scala
@@ -2,6 +2,7 @@ package org.mtkachev.stomp.server
 
 import org.specs._
 import org.specs.mock.Mockito
+import org.mockito.Matchers._
 
 import scala.actors.Actor
 
@@ -10,6 +11,7 @@ import org.apache.mina.core.session.IoSession
 
 import org.mtkachev.stomp.server.Subscriber._
 import org.mtkachev.stomp.server.codec._
+import org.hamcrest.{Description, BaseMatcher, Matcher}
 
 /**
  * User: mick
@@ -22,21 +24,21 @@ class SubscriberSpecification extends Specification with Mockito {
   private var dm: DestinationManager = null
   private var ioSession: IoSession = null
 
-  "queue manager" should {
+  "subscriber" should {
     doBefore {
       ioSession = mock[IoSession]
       dm = mock[DestinationManager]
-      subscriber = new Subscriber(dm, null, "foo", "bar")
+      subscriber = new Subscriber(dm, ioSession, "foo", "bar")
     }
     doAfter {
       subscriber ! Subscriber.Stop()
-      subscriber.getState must eventually(10, 1 second)(be(Actor.State.Terminated))
+      subscriber.getState must eventually(3, 1 second)(be(Actor.State.Terminated))
     }
-    "test subscrbe and unsubscribe" in {
+    "subscrbe and unsubscribe" in {
       subscriber ! FrameMsg(Subscribe(Some("foo"), "/foo/bar", true, Map.empty))
       subscriber ! FrameMsg(Subscribe(None, "/baz/ger", false, Map.empty))
 
-      subscriber.subscriptionList.size must eventually(10, 1 second)(be(2))
+      subscriber.subscriptionList.size must eventually(3, 1 second)(be(2))
 
       subscriber.subscriptionList mustContain new Subscription("/foo/bar", subscriber, true, Some("foo"))
       subscriber.subscriptionList mustContain new Subscription("/baz/ger", subscriber, false, None)
@@ -44,15 +46,54 @@ class SubscriberSpecification extends Specification with Mockito {
       there was one(dm) ! DestinationManager.Subscribe(new Subscription("/foo/bar", subscriber, true, Some("foo")))
       there was one(dm) ! DestinationManager.Subscribe(new Subscription("/baz/ger", subscriber, false, None))
 
-      println("subscribe test done")
-
       subscriber ! FrameMsg(UnSubscribe(Some("foo"), None, Map.empty))
       subscriber ! FrameMsg(UnSubscribe(None, Some("/baz/ger"), Map.empty))
 
-      subscriber.subscriptionList.size must eventually(10, 1 second)(be(0))
+      subscriber.subscriptionList.size must eventually(3, 1 second)(be(0))
 
       there was one(dm) ! DestinationManager.UnSubscribe(new Subscription("/foo/bar", subscriber, true, Some("foo")))
       there was one(dm) ! DestinationManager.UnSubscribe(new Subscription("/baz/ger", subscriber, false, None))
+      subscriber.getState must(be(Actor.State.Suspended))
     }
+    "send" in {
+      val content = "0123456789".getBytes
+      subscriber ! FrameMsg(Send("foo/bar", 10, None, content, Map.empty))
+      there was one(dm) ! DestinationManager.Message("foo/bar", 10, content)
+      subscriber.getState must(be(Actor.State.Suspended))
+    }
+    "receive" in {
+      subscriber ! FrameMsg(Subscribe(Some("foo"), "/foo/bar", false, Map.empty))
+      subscriber ! FrameMsg(Subscribe(None, "/baz/ger", false, Map.empty))
+      subscriber.subscriptionList.size must eventually(3, 1 second)(be(2))
+
+      val content = "0123456789".getBytes
+      subscriber ! Subscriber.Recieve(subscriber.subscriptionList(0), 10, content)
+      subscriber ! Subscriber.Recieve(subscriber.subscriptionList(1), 10, content)
+
+      def matchMessage(pattern: Message): Matcher[Message] = new BaseMatcher[Message] {
+        def matches(o: AnyRef) = {
+          val msg = o.asInstanceOf[Message]
+          msg.destination == pattern.destination && msg.contentLength == pattern.contentLength && msg.body == pattern.body
+        }
+        def describeTo(desc: Description) = {desc.appendText("should match " + pattern)}
+      }
+
+      there was one(ioSession).write(argThat(matchMessage(new Message("foo", "", 10, content, Map.empty))))
+      there was one(ioSession).write(argThat(matchMessage(new Message("/baz/ger", "", 10, content, Map.empty))))
+
+      subscriber.getState must(be(Actor.State.Suspended))
+    }
+/*
+    "ack" in {
+      subscriber ! FrameMsg(Subscribe(Some("foo"), "/foo/bar", true, Map.empty))
+      subscriber.subscriptionList.size must eventually(10, 1 second)(be(1))
+
+      val content = "0123456789".getBytes
+    }
+    "tx commit" in {
+    }
+    "tx rollback" in {
+    }
+*/
   }
 }
