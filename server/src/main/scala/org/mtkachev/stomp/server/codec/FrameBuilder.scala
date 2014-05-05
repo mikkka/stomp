c12606131ae6c17a8f8031ea5549ab7bee3416cb
mick <mick@z-inc.net>
2014-05-05 22:16:16 +0600

    - code clean;

--- server/src/main/scala/org/mtkachev/stomp/server/codec/FrameBuilder.scala
+++ server/src/main/scala/org/mtkachev/stomp/server/codec/FrameBuilder.scala
@@ -10,78 +10,67 @@ object FrameBuilder {
   def composeFrame(messageType: String, headers: Map[String, String], body: Array[Byte]) : Frame = {
     val receipt = headers.get("receipt")
     messageType match {
-      case "CONNECT" => {
+      case "CONNECT" =>
         (headers.get("login"), headers.get("passcode")) match {
           case (Some(login), Some(passcode)) =>
-            new Connect(login, passcode)
+            Connect(login, passcode)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "SEND" => {
+      case "SEND" =>
         (headers.get("destination"), headers.get("content-length"), headers.get("transaction")) match {
           case (Some(destination), Some(contentLength), transactionIdOpt) =>
-            new Send(destination, contentLength.toInt, transactionIdOpt, receipt, body)
+            Send(destination, contentLength.toInt, transactionIdOpt, receipt, body)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "SUBSCRIBE" => {
+      case "SUBSCRIBE" =>
         (headers.get("destination"), headers.get("ack"), headers.get("id")) match {
           case (Some(destination), Some("client"), id) =>
-            return new Subscribe(id, destination, true, receipt)
+            Subscribe(id, destination, ackMode = true, receipt)
           case (Some(destination), _, id) =>
-            return new Subscribe(id, destination, false, receipt)
+            Subscribe(id, destination, ackMode = false, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "UNSUBSCRIBE" => {
+      case "UNSUBSCRIBE" =>
         (headers.get("id"), headers.get("destination")) match {
           case (id, destination) if id != None || destination != None =>
-            return new UnSubscribe(id, destination, receipt)
+            UnSubscribe(id, destination, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "BEGIN" => {
-        (headers.get("transaction")) match {
-          case (Some(transactionId)) =>
-            return new Begin(transactionId, receipt)
+      case "BEGIN" =>
+        headers.get("transaction") match {
+          case Some(transactionId) =>
+            Begin(transactionId, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "COMMIT" => {
-        (headers.get("transaction")) match {
-          case (Some(transactionId)) =>
-            return new Commit(transactionId, receipt)
+      case "COMMIT" =>
+        headers.get("transaction") match {
+          case Some(transactionId) =>
+            Commit(transactionId, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "ACK" => {
+      case "ACK" =>
         (headers.get("message-id"), headers.get("transaction")) match {
           case (Some(messageId), transactionId: Option[String]) =>
-            return new Ack(messageId, transactionId, receipt)
+            Ack(messageId, transactionId, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "ABORT" => {
-        (headers.get("transaction")) match {
-          case (Some(transactionId)) =>
-            return new Abort(transactionId, receipt)
+      case "ABORT" =>
+        headers.get("transaction") match {
+          case Some(transactionId) =>
+            Abort(transactionId, receipt)
           case _ =>
             createErrorFrame(messageType, headers, body)
         }
-      }
-      case "DISCONNECT" => {
-        return new Disconnect(receipt)
-      }
-      case _ => {
-        createErrorFrame(messageType, headers, body)
-      }
+      case "DISCONNECT" =>
+        Disconnect(receipt)
+      case _ => createErrorFrame(messageType, headers, body)
     }
   }
 
