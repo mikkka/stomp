4efc12adcf1f5519527325afea7845d899829533
mick <mick@z-inc.net>
2014-08-22 19:49:47 +0600

    6:  - queue config read and exmple config

--- server/src/main/scala/org/mtkachev/stomp/server/Launcher.scala
+++ server/src/main/scala/org/mtkachev/stomp/server/Launcher.scala
@@ -4,13 +4,15 @@ import codec.{MainEventHandler, StompDecoder, StompEncoder}
 
 import io.netty.bootstrap.ServerBootstrap
 
+import scala.collection.JavaConversions._
 import io.netty.channel._
 import nio.NioEventLoopGroup
 import socket.nio.NioServerSocketChannel
 import socket.SocketChannel
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigObject, ConfigFactory}
 import com.typesafe.scalalogging.slf4j.StrictLogging
-import org.mtkachev.stomp.server.persistence.{StorePersisterWorker, InMemoryStore, Persister}
+import org.mtkachev.stomp.server.persistence._
+import java.io.File
 
 
 object Launcher extends App with StrictLogging {
@@ -18,6 +20,37 @@ object Launcher extends App with StrictLogging {
 
   val listenPort = config.getInt("listenPort")
   val queueDefaultSize = config.getInt("queue.size")
+  val preinitedDestConfigs = config.getConfig("queue").root().collect { case (k: String, v: ConfigObject) => k -> v}
+  val preinitedDests = preinitedDestConfigs.map { case (name, conf) =>
+    name -> {
+      val _conf = conf.toConfig
+
+      def getQueueConfOrDef(name: String) = if (_conf.hasPath(name)) _conf else config.getConfig("queue")
+      val t = getQueueConfOrDef("type").getString("type")
+      val queueSize = getQueueConfOrDef("size").getInt("size")
+
+      if (t == "mem") {
+        logger.info(s"init $name in memory destination")
+        new Destination(name, queueSize, new Persister(new StorePersisterWorker(new InMemoryStore)))
+      } else if (t == "fs") {
+        logger.info(s"init $name fs destination")
+        val workdir = getQueueConfOrDef("workdir").getString("workdir")
+        val queueSize = getQueueConfOrDef("size").getInt("size")
+        val journalChunk = getQueueConfOrDef("size").getInt("size")
+        val aheadChunk = getQueueConfOrDef("size").getInt("size")
+
+        val store = FSStore.journalAndAheadLogStore(new File(workdir), journalChunk, aheadChunk)
+        logger.info(s"reading prev messages...")
+        val prevMessages = store.init()
+        logger.info(s"done")
+
+        new Destination(name, queueSize,
+          new Persister(new BackgroundThreadPersisterWorker(new StorePersisterWorker(store))), prevMessages)
+      } else {
+        throw new IllegalArgumentException(s"unknown queue type $t")
+      }
+    }
+  }
 
   val destinationManager = new DestinationManager(destionationFactory)
   val subscriberManager = new SubscriberManager
@@ -27,12 +60,12 @@ object Launcher extends App with StrictLogging {
 
   val bootstrap = new ServerBootstrap()
   try {
-  bootstrap.
-    group(new NioEventLoopGroup(), new NioEventLoopGroup()).
-    channel(classOf[NioServerSocketChannel]).
-    childHandler(new StompServerInitializer(destinationManager, subscriberManager)).
-    childOption(ChannelOption.TCP_NODELAY.asInstanceOf[ChannelOption[Any]], true).
-    childOption(ChannelOption.SO_KEEPALIVE.asInstanceOf[ChannelOption[Any]], true)
+    bootstrap.
+      group(new NioEventLoopGroup(), new NioEventLoopGroup()).
+      channel(classOf[NioServerSocketChannel]).
+      childHandler(new StompServerInitializer(destinationManager, subscriberManager)).
+      childOption(ChannelOption.TCP_NODELAY.asInstanceOf[ChannelOption[Any]], true).
+      childOption(ChannelOption.SO_KEEPALIVE.asInstanceOf[ChannelOption[Any]], true)
 
     val ch = bootstrap.bind(listenPort).sync().channel()
     logger.info("stomp serv: starting on port " + listenPort)
@@ -52,7 +85,9 @@ object Launcher extends App with StrictLogging {
   }
 
   def destionationFactory(name: String): Destination = {
-    val queueSize = if (config.hasPath(s"queue.$name.size")) config.getInt(s"queue.$name.size") else queueDefaultSize
-    new Destination(name, queueSize, new Persister(new StorePersisterWorker(new InMemoryStore)))
+    preinitedDests.getOrElse(name, {
+      val queueSize = if (config.hasPath(s"queue.$name.size")) config.getInt(s"queue.$name.size") else queueDefaultSize
+      new Destination(name, queueSize, new Persister(new StorePersisterWorker(new InMemoryStore)))
+    })
   }
 }
