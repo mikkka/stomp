64019c01a6c77c951121a88736326ba953523d9c
mick <mick@z-inc.net>
2014-08-25 19:04:24 +0600

    6:  - client module

---
+++ client/build.sbt
@@ -0,0 +1,7 @@
+organization := "org.mtkachev"
+
+name := "stomp-client"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.1"
--- project/Build.scala
+++ project/Build.scala
@@ -2,9 +2,11 @@ import sbt._
 import Keys._
 
 object RootBuild extends Build {
-  lazy val root = Project(id = "root", base = file(".")).aggregate(server, common)
+  lazy val root = Project(id = "root", base = file(".")).aggregate(common, server, client)
+
+  lazy val common = Project(id = "stomp-common", base = file("common"))
 
   lazy val server = Project(id = "stomp-server", base = file("server")).dependsOn(common)
 
-  lazy val common = Project(id = "stomp-common", base = file("common"))
+  lazy val client = Project(id = "stomp-server", base = file("server")).dependsOn(common)
 }
